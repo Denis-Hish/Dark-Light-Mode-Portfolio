0f3179759a8af70178250fff55e5d9bb9e8da8e9
Denis-Hish <denis.hish@gmail.com>
2024-06-26 15:58:00 +0200

    fdbdgr

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.querySelector('.container');
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = 'dark-mode';
 document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove('active');
 
 const darkModeImg = document.querySelector('#dark-mode .home-img img');
+
+darkModeImg.src = 'img/imgDark.png';
